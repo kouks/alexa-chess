9d54c7f77e870ab8db7641e2530022e30cbcc64a
Timbo Cole <timbo_cole@yahoo.co.uk>
2016-12-04 03:38:56 +0000

    Update ChessBot.php

--- chess/ChessBot.php
+++ chess/ChessBot.php
@@ -1,11 +1,15 @@
 <?php
 class ChessBot {
     protected static $moveMap = [
-        "4-2" => [6,6,6,5],
+        "4-2" => [6,6,6,4],
+        "5-3" => [5,6,5,5],
     ];
     
     public static function init(ChessBoard $board, array $to) {
-        $move = static::$moveMap[implode("-", $to)];
-        $board->move([$move[0], $move[1]], [$move[2], $move[3]], True); 
+        if (isset(static::$moveMap[implode("-", $to)])) {
+            $move = static::$moveMap[implode("-", $to)];
+            $board->move([$move[0], $move[1]], [$move[2], $move[3]], True); 
+        }
+        
     }
 }
